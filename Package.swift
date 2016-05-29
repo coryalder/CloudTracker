a8ae963158cc2a34bf5e7b330ffd3fa5f09c5216
Cory Alder <coryalder@gmail.com>
2016-05-28 19:25:03 -0700

    Using my fork of FluentSQLite (fix for Swift 05-09)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
    dependencies: [
        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 9),
        .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 3),
-       .Package(url: "https://github.com/qutheory/fluent-sqlite.git", majorVersion: 0, minor: 3),
+       .Package(url: "https://github.com/coryalder/fluent-sqlite", majorVersion: 0, minor: 3),
        .Package(url: "https://github.com/zewo/uuid.git", majorVersion: 0, minor: 2)
    ]
 )
